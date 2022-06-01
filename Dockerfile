ce8b9308ea8a0c2806934d2ba5d4078665480380
Paula Moratalla <paulamrodriguez723@gmail.com>
2022-06-01 18:18:28 +0200

    error handleling and postman collection

--- Dockerfile
+++ Dockerfile
@@ -9,16 +9,3 @@ RUN echo "running"
 RUN npm install
 
 CMD ["npm", "start"]
-
-
-# FROM node:latest
-# ADD ./exercise01/bootcamp /app
-# WORKDIR /app
-# RUN ls -a
-# RUN npm install
-# RUN echo "hello world"
-# CMD ["npm", "start"]
-
-
-# // docker stop 
-# // docker kill 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,13 +3,26 @@ const messagesService = require("../services/message.service")
 
 router.post("/messages", (req, res, next) => {
 
-  const {destination, body} = req.body 
+  const { destination, message } = req.body
+  const body = message
 
   messagesService
-.sendMessage({destination, body})
-.then((message)=> res.status(200).json(message.data))
-.catch((err)=> res.status(500).json({message: "server failed"}))
+    .sendMessage({ destination, body })
+    .then((message) => res.status(200).json(message.data))
+    .catch((error) => {
 
+      if (!error.config.data.includes("destination") && !error.config.data.includes("body")) {
+        return res.status(405).json({ message: "Destination and body key are empty, it must have destination and body keys" })
+      }
+
+      if (!error.config.data.includes("destination")) {
+        return res.status(405).json({ message: "Destination key is empty, it must have destination and body keys" })
+      }
+      if (!error.config.data.includes("body")) {
+        return res.status(405).json({ message: "Body key is empty, it must have destination and body keys" })
+      }
+
+    })
 })
 
 module.exports = router;
