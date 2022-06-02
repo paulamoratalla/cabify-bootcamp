3ae6e795ab7a4bd1dc6b48376cd60aa6379ff551
Paula Moratalla <paulamrodriguez723@gmail.com>
2022-06-02 14:21:11 +0200

    problems from email solved

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,23 +6,33 @@ router.post("/messages", (req, res, next) => {
   const { destination, message } = req.body
   const body = message
 
+  if (!destination && !body) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "The keys can't be empty. The necessary keys are destination and message",
+      });
+  }
+  if (!destination) {
+    return res.status(400).json({
+      message:
+        "The key destination must exist. The necessary keys are destination and message",
+    });
+  }
+  if (!body) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "The key message must exist. The necessary keys are destination and message",
+      });
+  }
+
   messagesService
     .sendMessage({ destination, body })
     .then((message) => res.status(200).json(message.data))
-    .catch((error) => {
-
-      if (!error.config.data.includes("destination") && !error.config.data.includes("body")) {
-        return res.status(405).json({ message: "Destination and body key are empty, it must have destination and body keys" })
-      }
-
-      if (!error.config.data.includes("destination")) {
-        return res.status(405).json({ message: "Destination key is empty, it must have destination and body keys" })
-      }
-      if (!error.config.data.includes("body")) {
-        return res.status(405).json({ message: "Body key is empty, it must have destination and body keys" })
-      }
-
-    })
+    .catch(err => res.status(500).json(err))
 })
 
 module.exports = router;
